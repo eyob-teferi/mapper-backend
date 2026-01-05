0bc3094a9e522914265dbce54e3cf5a8dfe0c150
eyob-teferi <113609510+eyob-teferi@users.noreply.github.com>
2026-01-05 16:23:22 +0300

    Add comment for webhook checking
    Add a comment for checking the webhook

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+//checking the webhook
 const app = express();
 //testing webhook
 // Middleware
