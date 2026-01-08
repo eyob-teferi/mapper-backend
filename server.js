79b6f9c515d1c0be8b2dce16b2b66fc8f5369e7e
eyob-teferi <113609510+eyob-teferi@users.noreply.github.com>
2026-01-08 09:13:59 +0300

    Clean up comments in server.js
    Removed unnecessary comment from server.js.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+//what is going on?
 // Routes dsfsdfsd
 // app.use('/api/auth', require('./routes/auth')); // Comment out auth route
 app.use('/api/orders', require('./routes/orders'));
